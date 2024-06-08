02d01b8ee5c427594edf747979b1d54b55240231
scs0209 <tjdckdtn2463@naver.com>
2024-06-09 01:18:16 +0900

    feat: 제품 상세 페이지 퍼블 완성
    - 수량을 올리고 내릴수있는 Add 컴포넌트 퍼블 완성
    - 색상을 고를 수 있는 기능을 할 CustomizeProducts 컴포넌트 퍼블 완성

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from "@/components/common/Add";
+import CustomizeProducts from "@/components/common/CustomizeProducts";
 import ProductImages from "@/components/common/ProductImages";
 import React from "react";
 
@@ -9,7 +11,65 @@ const ProductPage = () => {
         <ProductImages />
       </div>
       {/* TEXT */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">Texts</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
+          sapien lorem. Donec vel nulla et lectus pharetra pretium. Aliquam erat
+          volutpat. Interdum et malesuada fames ac ante ipsum primis in
+          faucibus. Integer iaculis urna a iaculis interdum. Ut porta ex eros,
+          vel auctor ex varius sit amet. Quisque iaculis leo ut euismod
+          tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed aliquam
+          sed orci in ornare.
+        </p>
+        <hr className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h3 className="font-medium text-2xl">$49</h3>
+        </div>
+        <hr className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <hr className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt sapien lorem. Donec vel nulla et lectus pharetra pretium.
+            Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Integer iaculis urna a iaculis interdum. Ut
+            porta ex eros, vel auctor ex varius sit amet. Quisque iaculis leo ut
+            euismod tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed
+            aliquam sed orci in ornare.
+          </p>
+        </div>
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt sapien lorem. Donec vel nulla et lectus pharetra pretium.
+            Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Integer iaculis urna a iaculis interdum. Ut
+            porta ex eros, vel auctor ex varius sit amet. Quisque iaculis leo ut
+            euismod tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed
+            aliquam sed orci in ornare.
+          </p>
+        </div>
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt sapien lorem. Donec vel nulla et lectus pharetra pretium.
+            Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Integer iaculis urna a iaculis interdum. Ut
+            porta ex eros, vel auctor ex varius sit amet. Quisque iaculis leo ut
+            euismod tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed
+            aliquam sed orci in ornare.
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
