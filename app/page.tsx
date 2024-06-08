40c30fce6581f8cf98ddd3922ad4e50ffc3acf08
scs0209 <tjdckdtn2463@naver.com>
2024-06-08 21:04:38 +0900

    feat: 슬라이더 컴포넌트 생성 및 이미지 url 설정
    - Slider 컴포넌트에서 사용할 slideItems 배열을 constants 폴더에 추가
    - next.config.mjs에 이미지 url 설정
    - 자동 슬라이드 전환 및 수동 제어 기능이 있는 Slider 컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Slider from "@/components/common/Slider";
+
 const HomePage = () => {
   return (
-    <div className=''>HomePage</div>
-  )
-}
+    <div className="">
+      <Slider />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
