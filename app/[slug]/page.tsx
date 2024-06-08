a8db19abd3e2a29ebbc8e96ecc4fa114abcc6055
scs0209 <tjdckdtn2463@naver.com>
2024-06-09 00:37:26 +0900

    feat: ProductImage 컴포넌트 생성
    - 제품 페이지에 이미지를 표시하는 컴포넌트
    - 메인 이미지와 서브 이미지를 표시

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,17 @@
+import ProductImages from "@/components/common/ProductImages";
 import React from "react";
 
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  return (
+    <main className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMAGE */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/* TEXT */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">Texts</div>
+    </main>
+  );
 };
 
 export default ProductPage;
