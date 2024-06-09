0bbca473a3eb99d3f00937256cee2f09034f5414
scs0209 <tjdckdtn2463@naver.com>
2024-06-10 00:33:45 +0900

    feat: slug 인코딩 추가 및 제품 데이터를 동적으로 로드하도록 수정
    - 제품 페이지에서 slug 인코딩 처리 추가하여 한국어로 불러와도 에러
      안나게 수정
    - Wix API를 사용하여 slug를 기반으로 제품 데이터 쿼리
    - 제품 이미지, 설명, 가격 및 추가 정보 동적으로 로드
    - 제품 옵션 및 변형 선택 기능 추가
    - CustomizeProducts 및 ProductImages 컴포넌트 업데이트

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,74 +1,72 @@
+import React from "react";
+import { notFound } from "next/navigation";
+import { products } from "@wix/stores";
+
 import Add from "@/components/common/Add";
 import CustomizeProducts from "@/components/common/CustomizeProducts";
 import ProductImages from "@/components/common/ProductImages";
-import React from "react";
 
-const ProductPage = () => {
+import { wixClientServer } from "@/lib/wixClientServer";
+
+const ProductPage = async ({ params }: { params: { slug: string } }) => {
+  const wixClient = await wixClientServer();
+
+  const decodedSlug = decodeURIComponent(params.slug);
+
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", decodedSlug)
+    .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <main className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMAGE */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXT */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
-          sapien lorem. Donec vel nulla et lectus pharetra pretium. Aliquam erat
-          volutpat. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Integer iaculis urna a iaculis interdum. Ut porta ex eros,
-          vel auctor ex varius sit amet. Quisque iaculis leo ut euismod
-          tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed aliquam
-          sed orci in ornare.
-        </p>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
         <hr className="h-[2px] bg-gray-100" />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">$59</h3>
-          <h3 className="font-medium text-2xl">$49</h3>
-        </div>
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="text-xl text-gray-500 line-through">
+            ${product.price?.price}
+          </h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ${product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              ${product.price?.discountedPrice}
+            </h2>
+          </div>
+        )}
         <hr className="h-[2px] bg-gray-100" />
-        <CustomizeProducts />
+        {product.variants && product.productOptions && (
+          <CustomizeProducts
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        )}
         <Add />
         <hr className="h-[2px] bg-gray-100" />
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            tincidunt sapien lorem. Donec vel nulla et lectus pharetra pretium.
-            Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Integer iaculis urna a iaculis interdum. Ut
-            porta ex eros, vel auctor ex varius sit amet. Quisque iaculis leo ut
-            euismod tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed
-            aliquam sed orci in ornare.
-          </p>
-        </div>
-
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            tincidunt sapien lorem. Donec vel nulla et lectus pharetra pretium.
-            Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Integer iaculis urna a iaculis interdum. Ut
-            porta ex eros, vel auctor ex varius sit amet. Quisque iaculis leo ut
-            euismod tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed
-            aliquam sed orci in ornare.
-          </p>
-        </div>
-
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            tincidunt sapien lorem. Donec vel nulla et lectus pharetra pretium.
-            Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Integer iaculis urna a iaculis interdum. Ut
-            porta ex eros, vel auctor ex varius sit amet. Quisque iaculis leo ut
-            euismod tincidunt. Etiam molestie ullamcorper enim id pharetra. Sed
-            aliquam sed orci in ornare.
-          </p>
-        </div>
+        {product.additionalInfoSections?.map(
+          (section: products.AdditionalInfoSection) => (
+            <div className="text-sm" key={section.title}>
+              <h4 className="font-medium mb-4">{section.title}</h4>
+              <p>{section.description}</p>
+            </div>
+          )
+        )}
       </div>
     </main>
   );
--- components/common/CustomizeProducts.tsx
+++ components/common/CustomizeProducts.tsx
@@ -1,19 +1,124 @@
-import React from "react";
+"use client";
 
-const CustomizeProducts = () => {
+import React, { useState } from "react";
+import { products } from "@wix/stores";
+
+const CustomizeProducts = ({
+  productId,
+  variants,
+  productOptions,
+}: {
+  productId: string;
+  variants: products.Variant[];
+  productOptions: products.ProductOption[];
+}) => {
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: string;
+  }>({});
+
+  const isVariantInStock = (choices: { [key: string]: string }) => {
+    return variants.some((variant) => {
+      const variantChoices = variant.choices;
+      if (!variantChoices) return false;
+
+      return (
+        Object.entries(choices).every(
+          ([key, value]) => variantChoices[key] === value
+        ) &&
+        variant.stock?.inStock &&
+        variant.stock?.quantity &&
+        variant.stock.quantity > 0
+      );
+    });
+  };
+
+  const handleOptionSelect = (optionType: string, choice: string) => {
+    setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+  };
+
+  console.log(selectedOptions);
+
   return (
     <section className="flex flex-col gap-6">
-      <h4 className="font-medium">Choose a color</h4>
-      <ul className="flex items-center gap-3">
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
-          <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-        </li>
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500"></li>
-        <li className="w-8 h-8 rounded-full cursor-not-allowed ring-1 ring-gray-300 relative bg-green-500">
-          <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-        </li>
-      </ul>
-      <h4 className="font-medium">Choose a size</h4>
+      {productOptions?.map((option) => (
+        <div className="flex flex-col gap-4" key={option.name}>
+          <h4 className="font-medium">Choose a {option.name}</h4>
+          <ul className="flex items-center gap-3">
+            {option.choices?.map((choice) => {
+              const disabled = !isVariantInStock({
+                ...selectedOptions,
+                [option.name!]: choice.description!,
+              });
+
+              const selected =
+                selectedOptions[option.name!] === choice.description;
+
+              const clickHandler = disabled
+                ? undefined
+                : () => handleOptionSelect(option.name!, choice.description!);
+
+              return option.name === "색상" ? (
+                <li
+                  className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative"
+                  style={{
+                    backgroundColor: choice.value,
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {selected && (
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                  {disabled && (
+                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                </li>
+              ) : (
+                <li
+                  className="ring-1 ring-counter text-counter rounded-md py-1 px-4 text-sm cursor-pointer"
+                  style={{
+                    backgroundColor: selected
+                      ? "#02BE61"
+                      : disabled
+                      ? "#BBF7D0"
+                      : "white",
+                    cursor: disabled ? "not-allowed" : "pointer",
+                    color: selected || disabled ? "white" : "#02BE61",
+                    boxShadow: disabled ? "none" : "",
+                  }}
+                >
+                  {choice.description}
+                </li>
+                // <div
+                //   className=""
+                //   key={choice.value}
+                //   onClick={() =>
+                //     handleOPtionsSelect(option.name!, choice.description!)
+                //   }
+                // >
+                //   {choice.description}
+                //   {disabled && "disabled"}
+                //   {selected && "selected"}
+                // </div>
+              );
+            })}
+          </ul>
+        </div>
+      ))}
+      {/* COLOR */}
+      {/* 
+          <ul className="flex items-center gap-3">
+            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
+              <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+            </li>
+            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500"></li>
+            <li className="w-8 h-8 rounded-full cursor-not-allowed ring-1 ring-gray-300 relative bg-green-500">
+              <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+            </li>
+          </ul> */}
+
+      {/* OTHERS */}
+      {/* <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
         <li className="ring-1 ring-counter text-counter rounded-md py-1 px-4 text-sm cursor-pointer">
           Small
@@ -24,7 +129,7 @@ const CustomizeProducts = () => {
         <li className="ring-1 ring-green-200 text-white bg-green-200  rounded-md py-1 px-4 text-sm cursor-not-allowed">
           Large
         </li>
-      </ul>
+      </ul> */}
     </section>
   );
 };
